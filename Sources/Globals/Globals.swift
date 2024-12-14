aa10267aa12059c63e9bdfd2cba54adfff6b3f26
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2024-12-14 16:31:01 +0400

    build fix

--- Sources/Globals/Globals.swift
+++ Sources/Globals/Globals.swift
@@ -12,8 +12,7 @@ let windowWidth = 640.0
 let windowHeight = 640.0
 let wasdMovementRadius: Double = 40
 
-
 func loadMap() -> Tilemap {
-  let jsonData = try! Data(contentsOf: URL(fileURLWithPath: "/Users/tornike-mac/Development/SwiftRaylib/Map.json"))
+  let jsonData = try! Data(contentsOf: URL(fileURLWithPath: "/Users/tornikegomareli/Development/RaySwiftenstein/Map.json"))
   return try! JSONDecoder().decode(Tilemap.self, from: jsonData)
 }
